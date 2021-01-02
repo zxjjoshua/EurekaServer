fe544feffcd7ce498be4ae35ad486df0b275a928
Joshua <153996013@qq.com>
2021-01-02 17:06:41 -0600

    add test api

--- src/main/java/com/EurekaServerApp.java
+++ src/main/java/com/EurekaServerApp.java
@@ -3,12 +3,20 @@ package com;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
 @SpringBootApplication
 @EnableEurekaServer
+@RestController
 public class EurekaServerApp {
     public static void main(String[] args){
         SpringApplication.run(EurekaServerApp.class, args);
+    }
+
+    @GetMapping("/index")
+    public String getIndex(){
+        return "this is index";
     }
 }
